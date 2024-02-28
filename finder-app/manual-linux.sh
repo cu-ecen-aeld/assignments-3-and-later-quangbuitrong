4a67aa5633a5dc8493c0767102c263e03693e530
quangbuitrong <trongquang190897@gmail.com>
2024-02-28 11:11:26 +0700

    assignment 3 implementation: part 2 - finder-app/manual-linux.sh

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -2,6 +2,15 @@
 # Script outline to install and build kernel.
 # Author: Siddhant Jajoo.
 
+# Note:
+# - Install some required packages before running this script:
+#   $ sudo apt install make build-essential libncurses-dev bison flex libssl-dev libelf-dev
+# - For the error: "multiple definition of yylloc"
+#   -> the error come from gcc-11 version, so to fix that, downgrade gcc version to gcc-9
+#   command for downgrade gcc version:
+#   $ sudo apt install gcc-9
+#   $ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 9
+
 set -e
 set -u
 
@@ -35,9 +44,16 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    echo "kernel build steps:"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j2 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j2 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +64,11 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +77,64 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+echo "Add library dependencies to rootfs"
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+INTERPRETER=$(find $SYSROOT -name "ld-linux-aarch64.so.1")
+cp ${INTERPRETER} ${OUTDIR}/rootfs/lib
+SHARED_LIB_1=$(find $SYSROOT -name "libm.so.6")
+cp ${SHARED_LIB_1} ${OUTDIR}/rootfs/lib64
+SHARED_LIB_2=$(find $SYSROOT -name "libresolv.so.2")
+cp ${SHARED_LIB_2} ${OUTDIR}/rootfs/lib64
+SHARED_LIB_3=$(find $SYSROOT -name "libc.so.6")
+cp ${SHARED_LIB_3} ${OUTDIR}/rootfs/lib64
 
 # TODO: Make device nodes
+echo "Make device nodes"
+cd ${OUTDIR}/rootfs
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/tty c 5 1
 
 # TODO: Clean and build the writer utility
+echo "Clean and build the writer utility"
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE} writer
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+echo "Copy the finder related scripts and executables to the /home directory \
+    on the target rootfs"
+cp -f writer ${OUTDIR}/rootfs/home
+cp finder.sh ${OUTDIR}/rootfs/home
+cp finder-test.sh ${OUTDIR}/rootfs/home
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home
+mkdir ${OUTDIR}/rootfs/home/conf
+cp conf/assignment.txt ${OUTDIR}/rootfs/home/conf
+cp conf/username.txt ${OUTDIR}/rootfs/home/conf
 
 # TODO: Chown the root directory
+echo "Chown the root directory"
+sudo chown -R root:root ${OUTDIR}/rootfs
 
 # TODO: Create initramfs.cpio.gz
+echo "Create initramfs.cpio.gz"
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
